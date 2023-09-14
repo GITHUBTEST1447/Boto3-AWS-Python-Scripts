97b23db9fa2aaf5e83311b9c4220395dc0ea39e3
SteffenP <pfahnl.steffen@gmail.com>
2023-09-13 20:07:20 -0500

    Finished create_alb()

--- create_alb+asg.py
+++ create_alb+asg.py
@@ -25,13 +25,34 @@ def get_public_subnets(vpc_id, region_name='us-east-1'):
                     break
 
     return public_subnets
+
 # Function to create ALB
 def create_alb(vpc_id):
-    public_subnets = get_public_subnets(vpc_id)
-    print(public_subnets)
 
-    #alb_response = alb_client.create_load_balancer(Name='Python ALB', Type='application', Subnets=subnetIDs)
+    # Get public subnets from helper
+    public_subnets = get_public_subnets(vpc_id)
+    
+    # Create Security Group for ALB
+    response = ec2_client.create_security_group(GroupName='PYTHON-ALB-SG', Description='SG created for Python ALB', VpcId=vpc_id)
+    security_group_id = response['GroupId']
+    ec2_client.create_tags(Resources=[security_group_id],Tags=[{'Key': 'Name', 'Value': 'MyTagName'}])
+    ec2_client.authorize_security_group_ingress(GroupId=security_group_id, IpProtocol='tcp', CidrIp='0.0.0.0/0', FromPort=80, ToPort=80)
+
+    # Create Load Balancer
+    response = alb_client.create_load_balancer(Name='PYTHON-ALB', Subnets=public_subnets, SecurityGroups=[security_group_id], Scheme='internet-facing', Type='application')
+    load_balancer_arn = response['LoadBalancers'][0]['LoadBalancerArn']
+
+    # Create Target Group and Listener for ALB
+    response = alb_client.create_target_group(Name='PYTHON-ALB-TG', Protocol='HTTP', Port=80, VpcId=vpc_id)
+    target_group_arn = response['TargetGroups'][0]['TargetGroupArn']
+    alb_client.create_listener(LoadBalancerArn=load_balancer_arn, Protocol='HTTP', Port=80, DefaultActions=[{'Type': 'forward', 'TargetGroupArn': target_group_arn}])
+    
+    return {
+    "SecurityGroupId": security_group_id,
+    "LoadBalancerArn": load_balancer_arn,
+    "TargetGroupArn": target_group_arn
+    }
 
 if __name__ == "__main__":
-    vpc_id = input()
+    vpc_id = input("Input VPC ID: ")
     create_alb(vpc_id)
